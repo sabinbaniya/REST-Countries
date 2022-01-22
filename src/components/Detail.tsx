640ca5f414ddc23d34e52b0af3552e8d8971184f
Sabin Baniya <dummymail@tutanota.de>
2022-01-22 14:51:53 +0545

    Responsive detail complete

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -31,7 +31,7 @@ const Detail: FC = () => {
                     </div>
                 </Link>
             </div>
-            <div>
+            <div className="">
                     {
                         detail &&
                         detail?.map(item => {
@@ -43,48 +43,50 @@ const Detail: FC = () => {
 
                             return (
                                 <div key={item.altSpellings[1]} className="flex flex-wrap dark:text-White text-LightVeryDarkBlue">
-                                    <div>
-                                        <img src={item?.flags.svg} alt={item.altSpellings[0]} />
+                                    <div className="basis-2/4">
+                                        <img className="max-h-96 max-w-sm" src={item?.flags.svg} alt={item.altSpellings[0]} />
                                     </div>
                                     <div className="my-10">
                                         <div>
                                             <p className="text-2xl font-bold mb-8">{country}</p>
                                         </div>
-                                        <div className="mb-6">
-                                            <p className="font-semibold mb-2">Native Name : <span className="font-light">{item.altSpellings[1]}</span></p>
-                                            <p className="font-semibold mb-2">Population : <span className="font-light">{item.population.toLocaleString()}</span></p>
-                                            <p className="font-semibold mb-2">Region : <span className="font-light">{item.region}</span></p>
-                                            <p className="font-semibold mb-2">Sub Region : <span className="font-light">{item.subregion}</span></p>
-                                            <p className="font-semibold mb-4">Capital : <span className="font-light">{item.capital.map(a => <span>{a}</span>)}</span></p>
-                                        </div>
-                                        <div className="mb-6">
-                                            <p className="font-semibold mb-2">Top Level Domain : <span className="font-light">{item.tld}</span></p>
-                                            <p className="font-semibold mb-2">Currencies : <span className="font-light">
-                                                {
-                                                    arr.map((item, index) => {
-                                                        return (
-                                                            <span>
-                                                                {obj[item]?.name} 
-                                                            </span>
-                                                        )
-                                                    })
-                                                }
-                                            </span></p>
-                                            <p className="font-semibold ">Languages : <span className="font-light">
-                                                {
-                                                    arr2.map((item, index) => {
-                                                        return (
-                                                            <span>
-                                                                {obj2[item]} 
-                                                            </span>
-                                                        )
-                                                    })
-                                                }
-                                            </span></p>
-                                        </div>
-                                        <div>
+                                        <div className="xl:flex">
+                                            <div className="mb-6">
+                                                <p className="font-semibold mb-2">Native Name : <span className="font-light pr-4">{item.altSpellings[1]}</span></p>
+                                                <p className="font-semibold mb-2">Population : <span className="font-light">{item.population.toLocaleString()}</span></p>
+                                                <p className="font-semibold mb-2">Region : <span className="font-light">{item.region}</span></p>
+                                                <p className="font-semibold mb-2">Sub Region : <span className="font-light">{item.subregion}</span></p>
+                                                <p className="font-semibold mb-4">Capital : <span className="font-light">{item.capital.map(a => <span>{a}</span>)}</span></p>
+                                            </div>
+                                            <div className="mb-6">
+                                                <p className="font-semibold mb-2">Top Level Domain : <span className="font-light">{item.tld}</span></p>
+                                                <p className="font-semibold mb-2">Currencies : <span className="font-light">
+                                                    {
+                                                        arr.map((item, index) => {
+                                                            return (
+                                                                <span>
+                                                                    {obj[item]?.name} 
+                                                                </span>
+                                                            )
+                                                        })
+                                                    }
+                                                </span></p>
+                                                <p className="font-semibold ">Languages : <span className="font-light">
+                                                    {
+                                                        arr2.map((item, index) => {
+                                                            return (
+                                                                <span>
+                                                                    {obj2[item]} 
+                                                                </span>
+                                                            )
+                                                        })
+                                                    }
+                                                </span></p>
+                                            </div>
+                                        </div>    
+                                        <div className="md:flex md:items-center ">
                                             <p className="font-semibold">Border Countries: </p>
-                                            <div className="font-light my-4">{item.borders.map(a => <span className="py-2 px-4 dark:bg-DarkBlue dark:text-White bg-White text-LightVeryDarkBlue shadow-xl">{a}</span>)}</div>
+                                            <div className="font-light my-4 md:mx-4 flex flex-wrap">{item.borders.map(a => <span className="py-2 px-4 mr-4 last:mr-0 md:mx-4 dark:bg-DarkBlue dark:text-White bg-White text-LightVeryDarkBlue shadow-xl">{a}</span>)}</div>
                                         </div>
                                     </div>
 
